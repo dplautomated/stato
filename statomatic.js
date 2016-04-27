221da2b74b0dbf3a2bc597a32643c85451b7b947
DPL <dplautomated@gmail.com>
2016-04-27 11:49:10 -0500

    Using correct version of statomatic now

--- statomatic.js
+++ statomatic.js
@@ -6,6 +6,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var morgan = require('morgan');
+var crypto = require('crypto');
 
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
@@ -17,12 +18,24 @@ var pool = mysql.createPool({
   database : 'stato_dentonpl_com'
 });
 
+
+//Everything in here is specifically to enable CORS for testing
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Accept");
-  res.header("Content-Type", "application/json");
-  next();
+  //res.header("Access-Control-Allow-Origin", "*");
+  ////res.header("Access-Control-Allow-Origin", "http://localhost");
+  ////res.header("Access-Control-Allow-Credentials", "true");
+  //res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Accept, x-access-token");
+  //res.header("Content-Type", "application/json");
+  //next();
+  
+  // intercept OPTIONS method  THIS IS EXTREMELY IMPORTANT.  Not doing this always leads "complex" methods to freak out.
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
 });
 
 
@@ -31,6 +44,8 @@ app.use(bodyParser.json());
 
 //Use morgan to log requests
 app.use(morgan('dev'));
+
+//Create 
 
 function reportError(err, res){
 	res.status(404);
@@ -44,8 +59,9 @@ function returnResult(res, rows){
 	res.end(JSON.stringify(rows));
 }
 
-var statomaticRouter = express.Router();
-
+var computerLogRouter = express.Router();
+var computerLogHistoryRouter = express.Router();
+var roomLogRouter = express.Router();
 
 app.get('/health', function(req, res)
 {
@@ -53,35 +69,74 @@ app.get('/health', function(req, res)
     res.end();
 });
 
+app.post('/register', function(req, res) {
+	if(req.body.username && req.body.email && req.body.password){
+	
+		pool.query('SELECT * FROM users WHERE username = ?', [req.body.username], function(err, rows, fields) {				
+		  if(err){
+				reportError(err, res);
+				return;
+			}
+			
+			if(rows.length > 0){
+				res.json({success: false, message:'Username not available, please pick another username.'});
+			}
+			else{
+				var salt = crypto.randomBytes(256).toString('hex');
+				var hashedpass = crypto.createHash("sha256")
+				  .update(salt)
+				  .update(req.body.password)
+				  .digest('hex');
+				
+				pool.query('INSERT INTO users (username, email, role, password, password_s) VALUES (?, ?, ?, ?, ?) ', [req.body.username, req.body.email, 'user', hashedpass, salt], function(err, rows, fields) {
+					if(err){
+						reportError(err, res);
+						return;
+					}
+					
+					res.json({success: true, message:'User created.'});
+				});
+			}
+			
+		});
+	}
+	else{
+	
+	//Do something better here and on everything else that's expecting certain parameters that aren't caught by the router.  Otherwise, it may get in and throw an error when something isn't provided.
+		console.log("bad");
+		res.sendStatus(404);
+	}
+});
+
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
-statomaticRouter.post('/login', function(req, res) {
+app.post('/login', function(req, res) {
 
 	pool.query('SELECT * FROM users WHERE username = ? AND active = 1', [req.body.username], function(err, rows, fields) {				
 	  if(err){
 			reportError(err, res);
 			return;
 		}
-		
-		/*
-	  var hashedpass = = crypto.createHash("sha256")
-	  .update(rows[0][1])
-      .update(password)
-      .digest('hex');
-	*/
-	console.log(rows);
-	console.log(rows.length);
 	
 	if (rows.length == 0) {
 		res.json({ success: false, message: 'Username not found.' });
+		return;
 	}
-	else if (rows[0]['password1'] != req.body.password){
+	
+	var hashedpass = crypto.createHash("sha256")
+			  .update(rows[0]['password_s'])
+			  .update(req.body.password)
+			  .digest('hex');
+	
+	if (rows[0]['password'] != hashedpass){
 		res.json({ success: false, message: 'Incorrect password.' });
+		return;
 	}
 	else{		
 		// if user is found and password is right
 		// create a token
-		var token = jwt.sign({username: rows[0]['username'], id: rows[0]['id']}, /*app.get('superSecret')*/'lexicallibrarian', {
-		expiresInMinutes: 720 // expires in 12 hours
+		var token = jwt.sign({username: rows[0]['username'], branch: rows[0]['branch'], role: rows[0]['role']}, /*app.get('superSecret')*/'lexicallibrarian', {
+		//expiresInMinutes: 720 // expires in 12 hours
+		expiresIn: 480
 		});
 
 		// return the information including token as JSON
@@ -93,63 +148,52 @@ statomaticRouter.post('/login', function(req, res) {
 	}
 	  
 	});
-/*
-  // find the user
-  User.findOne({
-    name: req.body.name
-  }, function(err, user) {
+});
 
-    if (err) throw err;
+app.use(function(req, res, next) {
 
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
+  // check header or url parameters or post parameters for token
+  var token = /*req.body.token || req.query.token ||*/ req.headers['x-access-token'];
 
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, /*app.get('superSecret')*/'lexicallibrarian', function(err, decoded) {      
+      if (err) {
+		console.log("error " + err);
+			return res.status(403).send({ 
+			success: false, 
+			message: 'Token expired.' 
+		});   
       } else {
+        // if everything is good, save to request for use in other routes
+        console.log("Decoded token: " + JSON.stringify(decoded));
+		req.decoded = decoded;    
+        next();
+      }
+    });
 
-        // if user is found and password is right
-        // create a token
-        var token = jwt.sign(user, app.get('superSecret'), {
-          expiresInMinutes: 1440 // expires in 24 hours
-        });
+  } else {
 
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
-
-    }
-
-  });
-  */
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
 });
 
-statomaticRouter.get('/computer/:branch', function(req, res) {
-	pool.query('SELECT * FROM computer_checkout_list WHERE branch = ? AND checkout = 1 AND status = 2', [req.params.branch], function(err, rows, fields) {				
+app.get('/computer/list/:branch', function(req, res) {
+	pool.query('SELECT * FROM computer_list WHERE branch = ? AND checkout = 1 AND status = 2', [req.params.branch], function(err, rows, fields) {				
 		returnResult(res, rows);
 	});
 });
 
-statomaticRouter.get('/computer/status/:branch', function(req, res) {
-	pool.query('SELECT res.id, res.name, res.branch, logs.id AS log_id, logs.pat_name, logs.time_out, logs.time_in FROM (SELECT * FROM computer_checkout_log WHERE DATE(time_out) = CURDATE()) AS logs JOIN (SELECT * FROM computer_checkout_list WHERE branch = ? AND checkout = 1 AND status = 2) AS res ON logs.resource_id = res.id ORDER BY res.name', [req.params.branch], function(err, rows, fields) {				
-		if(err){
-			reportError(err, res);
-			return;
-		}
-		
-		console.log("getting computer status");
-		returnResult(res, rows);
-	});
-});
-
-statomaticRouter.get('/computer/last/:branch', function(req, res) {
-	pool.query('SELECT res.name FROM (SELECT * FROM computer_checkout_log LIMIT 40) AS logs JOIN (SELECT * FROM computer_checkout_list WHERE branch = ? AND checkout = 1) AS res ON logs.resource_id = res.id ORDER BY logs.time_out DESC LIMIT 1', [req.params.branch], function(err, rows, fields) {				
+app.get('/computer/last/:branch', function(req, res) {
+	pool.query('SELECT res.name FROM (SELECT * FROM computer_checkout_log LIMIT 40) AS logs JOIN (SELECT * FROM computer_list WHERE branch = ? AND checkout = 1) AS res ON logs.resource_id = res.id ORDER BY logs.time_out DESC LIMIT 1', [req.params.branch], function(err, rows, fields) {				
 		if(err){
 			reportError(err, res);
 			return;
@@ -160,7 +204,19 @@ statomaticRouter.get('/computer/last/:branch', function(req, res) {
 	});
 });
 
-statomaticRouter.post('/computer', function(req, res) {
+app.get('/computer/status/:branch', function(req, res) {
+	pool.query('SELECT res.id, res.name, res.branch, logs.id AS log_id, logs.pat_name, logs.time_out, logs.time_in FROM (SELECT * FROM computer_checkout_log WHERE DATE(time_out) = CURDATE()) AS logs JOIN (SELECT * FROM computer_list WHERE branch = ? AND checkout = 1 AND status = 2) AS res ON logs.resource_id = res.id ORDER BY res.name', [req.params.branch], function(err, rows, fields) {				
+		if(err){
+			reportError(err, res);
+			return;
+		}
+		
+		console.log("getting computer status");
+		returnResult(res, rows);
+	});
+});
+
+computerLogRouter.post('/', function(req, res) {
 	pool.query('UPDATE computer_checkout_log SET time_in = NOW() WHERE resource_id = ? AND time_in IS NULL', [req.body.resource_id], function(err, rows, fields) {
 		if(err){
 			reportError(err, res);
@@ -178,7 +234,7 @@ statomaticRouter.post('/computer', function(req, res) {
 	});
 });
 
-statomaticRouter.put('/computer/:log_id', function(req, res) {
+computerLogRouter.put('/:log_id', function(req, res) {
 	pool.query('UPDATE computer_checkout_log SET time_in = NOW() WHERE time_in IS NULL AND id = ?', [req.params.log_id], function(err, rows, fields) {
 		if(err){
 			reportError(err, res);
@@ -190,7 +246,7 @@ statomaticRouter.put('/computer/:log_id', function(req, res) {
 	});
 });
 
-statomaticRouter.delete('/computer/:log_id', function(req, res) {
+computerLogRouter.delete('/:log_id', function(req, res) {
 	console.log(req.params.log_id);
 	pool.query('DELETE FROM computer_checkout_log WHERE id = ?', [req.params.log_id], function(err, rows, fields) {
 		if(err){
@@ -203,7 +259,8 @@ statomaticRouter.delete('/computer/:log_id', function(req, res) {
 	});
 });
 
-statomaticRouter.get('/patinfo/:barcode', function(req, res) {
+
+app.get('/patinfo/:barcode', function(req, res) {
 	request({url: 'https://library.cityofdenton.com/iii/sierra-api/v2/token', method: 'POST', headers: {'Authorization': 'Basic R2lXcW85M04vYlZIUjU1S0FLL3dQK2xMUE4xdTpqb2ZlNnljYw=='}}, function(error, request, body){
 		if(request.statusCode == '200'){
 			//var accesscode = 'Bearer ' + body.access_token;
@@ -229,7 +286,39 @@ statomaticRouter.get('/patinfo/:barcode', function(req, res) {
 
 });
 
-statomaticRouter.get('/room/:branch', function(req, res) {
+/*
+app.get('/patinfo/:barcode', function(req, res) {
+	var token = null;
+	request({url: 'https://library.cityofdenton.com/iii/sierra-api/v2/token', method: 'POST', headers: {'Authorization': 'Basic R2lXcW85M04vYlZIUjU1S0FLL3dQK2xMUE4xdTpqb2ZlNnljYw=='}}, function(error, request, body){
+		if(request.statusCode == '200'){
+			//var accesscode = 'Bearer ' + body.access_token;
+			token = JSON.parse(body).access_token;
+		}
+		else{
+			console.log(request.statusCode);
+			console.log(body);
+			return;
+		}
+	});
+	
+	if(token){
+		request({url: 'https://library.cityofdenton.com/iii/sierra-api/v2/patrons/find?barcode=' + req.params.barcode + '&fields=names%2CexpirationDate%2CmoneyOwed', method: 'GET', headers: {'Authorization': 'Bearer ' + token}}, function(error, request, body){
+			console.log('made new request');
+			if(request.statusCode == '200'){
+				console.log('gotit');
+				res.status(200);
+				res.end(body);
+			}
+			else{
+				res.status(400);
+				res.end(body);
+			}
+		});
+	}
+
+});
+*/
+roomLogRouter.get('/:branch', function(req, res) {
 	pool.query('SELECT res.id, res.name, res.branch, logs.id AS log_id, logs.number_people, logs.time_in, logs.time_out  FROM (SELECT * FROM room_list WHERE branch = ? AND active = 1) AS res LEFT JOIN (SELECT * FROM study_room_log WHERE time_out IS NULL and DATE(time_in) = CURDATE()) AS logs ON res.id = logs.resource_id ORDER BY res.name', [req.params.branch, 'study_room'], function(err, rows, fields) {
 		if(err){
 			reportError(err, res);
@@ -242,7 +331,7 @@ statomaticRouter.get('/room/:branch', function(req, res) {
 	});
 });
 
-statomaticRouter.post('/room', function(req, res) {
+roomLogRouter.post('/', function(req, res) {
 	pool.query('UPDATE study_room_log SET time_out = NOW() WHERE time_out IS NULL AND resource_id = ?', [req.body.resource_id], function(err, rows, fields) {
 		if(err){
 			reportError(err, res);
@@ -251,9 +340,7 @@ statomaticRouter.post('/room', function(req, res) {
 		else{
 			console.log("update out times");
 			pool.query('INSERT INTO study_room_log (resource_id, number_people) VALUES (?, ?)', [req.body.resource_id, req.body.number_people], function(err, rows, fields) {
-
 				if(err){
-					console.log("loggingerror");
 					reportError(err, res);
 					return;
 				}
@@ -265,7 +352,8 @@ statomaticRouter.post('/room', function(req, res) {
 	});
 });
 
-statomaticRouter.put('/room/:log_id', function(req, res) {
+roomLogRouter.put('/:log_id', function(req, res) {
+	console.log(req.params.log_id);
 	pool.query('UPDATE study_room_log SET time_out = NOW() WHERE time_out IS NULL AND id = ?', [req.params.log_id], function(err, rows, fields) {
 		if(err){
 			reportError(err, res);
@@ -278,7 +366,7 @@ statomaticRouter.put('/room/:log_id', function(req, res) {
 	});
 });
 
-statomaticRouter.delete('/room/:log_id', function(req, res) {
+roomLogRouter.delete('/:log_id', function(req, res) {
 	pool.query('DELETE FROM study_room_log WHERE id = ?', [req.params.log_id], function(err, rows, fields) {
 		if(err){
 			reportError(err, res);
@@ -291,7 +379,90 @@ statomaticRouter.delete('/room/:log_id', function(req, res) {
 	});
 });
 
-app.use('/', statomaticRouter);
+app.get('/computer/history/', function(req, res) {
+	
+	/*if(!req.query.from || !req.query.until)
+	{
+		res.sendStatus(404);
+	}
+	*/
+	console.log(req.query.from);
+	//var from_date = new Date(req.query.from);
+	//var until_date = new Date(req.query.until);
+	var from = moment(req.query.from).format('YYYY-MM-DD');
+	var until = moment(req.query.until).format('YYYY-MM-DD');
+	
+	pool.query('SELECT cl.branch, cl.name, COUNT(ccl.resource_id) AS checkouts FROM (SELECT resource_id FROM computer_checkout_log WHERE DATE(time_out) >= ? AND DATE(time_out) <= ?) AS ccl JOIN computer_list AS cl ON cl.id = ccl.resource_id GROUP BY cl.branch, cl.name WITH ROLLUP', [from, until], function(err, rows, fields) {
+		if(err){
+			reportError(err, res);
+			return;
+		}
+		else{
+			console.log("Fetching checkout totals per branch");
+			returnResult(res, rows);
+		}
+	});
+
+/*
+SELECT cl.name, COUNT(ccl.resource_id) AS checkouts
+FROM `computer_checkout_log` AS ccl
+JOIN `computer_list` AS cl
+ON cl.id = ccl.resource_id
+GROUP BY cl.name WITH ROLLUP
+
+ name 	branch 	checkouts 	
+delta 	north 	2
+echo 	north 	1
+foxtrot 	north 	6
+golf 	north 	4
+NULL	north 	13
+
+SELECT cl.name, cl.branch, COUNT( ccl.resource_id ) AS checkouts
+FROM `computer_checkout_log` AS ccl
+JOIN `computer_list` AS cl ON cl.id = ccl.resource_id
+GROUP BY cl.branch, cl.name
+WITH ROLLUP
+LIMIT 0 , 30
+
+ name 	branch 	checkouts 	
+echo 	north 	1
+foxtrot 	north 	6
+golf 	north 	4
+NULL	north 	11
+delta 	south 	2
+NULL	south 	2
+NULL	NULL	13
+
+*/
+});
+
+app.get('/room/history/', function(req, res) {
+	
+	/*if(!req.query.from || !req.query.until)
+	{
+		res.sendStatus(404);
+	}
+	*/
+	console.log(req.query.from);
+	//var from_date = new Date(req.query.from);
+	//var until_date = new Date(req.query.until);
+	var from = moment(req.query.from).format('YYYY-MM-DD');
+	var until = moment(req.query.until).format('YYYY-MM-DD');
+	
+	pool.query('SELECT rl.branch, rl.name, COUNT(srl.resource_id) AS checkouts FROM (SELECT resource_id FROM study_room_log WHERE DATE(time_in) >= ? AND DATE(time_in) <= ?) AS srl JOIN room_list AS rl ON rl.id = srl.resource_id GROUP BY rl.branch, rl.name WITH ROLLUP', [from, until], function(err, rows, fields) {
+		if(err){
+			reportError(err, res);
+			return;
+		}
+		else{
+			console.log("Fetching room totals per branch");
+			returnResult(res, rows);
+		}
+	});
+});
+
+app.use('/computer/log', computerLogRouter);
+app.use('/room/log', roomLogRouter);
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP, function(){console.log("Sever started!")});
 
